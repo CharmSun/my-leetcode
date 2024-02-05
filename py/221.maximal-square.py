edcccb47821a1eb5345503a25b31c20ad5102afe
suncan <suncan@taou.com>
2024-02-05 16:56:56 +0800

    update: 221

--- py/221.maximal-square.py
+++ py/221.maximal-square.py
@@ -13,12 +13,14 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0]) if rows > 0 else 0
         maxLen = 0
-        dp = [[0] * (cols+1) for i in range(rows+1)]
-        for i in range(1, rows+1):
-            for j in range(1, cols+1):
-                if matrix[i-1][j-1] == '1':
+        dp = [[0] * cols for i in range(rows)]
+        for i in range(0, rows):
+            for j in range(0, cols):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1 if matrix[i][j] == '1' else 0
+                elif matrix[i][j] == '1':
                     dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-                    maxLen = max(maxLen, dp[i][j])
+                maxLen = max(maxLen, dp[i][j])
         return maxLen ** 2
         
 # @lc code=end
